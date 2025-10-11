e1802ace2560de4a80d2bdd4ff0c4436359a39c1
Hassan <hassan.bellounis@gmail.com>
2025-10-11 19:30:19 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -815,12 +815,6 @@ require('lazy').setup({
     'savq/melange-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('melange').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
       vim.cmd.colorscheme 'melange'
     end,
   },
